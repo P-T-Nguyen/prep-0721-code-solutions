e8d3497a10bd0703fe605b97efccb761a0e0cf1b
Peter Nguyen <petert.nguyen@gmail.com>
2021-08-12 22:52:48 -0700

    Completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var language = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', language);
+
+var firstElement = language[0];
+console.log('the first element of the array is', firstElement);
+
+var thirdElement = language[2];
+console.log('the 3rd element of the array is', thirdElement);
+
+var length = language.length;
+console.log('The Length of the array is', length);
+
+var lastElement = language[language.length - 1];
+console.log('The last element of the array is', lastElement);
