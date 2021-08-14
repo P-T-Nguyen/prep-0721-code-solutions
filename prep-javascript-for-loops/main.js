4791ee73ae6c1718de9af47c29c8699c0585e51f
Peter Nguyen <petert.nguyen@gmail.com>
2021-08-14 03:54:12 -0700

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 20; i = i + 2) {
+  console.log(i);
+}
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion:' + i);
+}
