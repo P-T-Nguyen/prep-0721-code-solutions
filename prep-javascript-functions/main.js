652ab75b6afd3ae3fc44bf085028e8b5e4cfda4c
Peter Nguyen <petert.nguyen@gmail.com>
2021-08-14 00:27:50 -0700

    Completed prep-javascript-functions-1of10

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -5,3 +5,15 @@ function addTwoNumbers(num1, num2) {
 
 var addTwoNumbersResults = addTwoNumbers(2, 2);
 console.log('addTwoNumbers Exercise:', addTwoNumbersResults);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var convertHoursToMinutesResults = convertHoursToMinutes(2);
+console.log('convertHoursToMintutes Excercise:', convertHoursToMinutesResults);
+
+function getGreeting(name) {
+  return name;
+}
+var getGreetingResult = getGreeting('World');
+console.log('getGreetingResult Excercise: Hello' + ' ' + getGreetingResult + '!');
